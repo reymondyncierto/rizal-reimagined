245ec111732a0ca5a5df1f288eb018eef9f410b6
Reymond Yncierto <rpyncierto@up.edu.ph>
2025-06-02 09:47:34 +0800

    chore: UPDATE footer

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -4,11 +4,11 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <div className="mt-6 text-center text-gray-500 text-sm rounded-lg p-6">
           <div className="mb-4">
-            <p>This exploration was created to promote critical thinking about the Philippine history and society, examining how they align with the principles and ideals taught by José Rizal.</p>
-            <p className="mt-1">All Rizal quotes are from public domain works. Modern examples are cited from credible news sources.</p>
+            <p>This exploration was created to promote critical thinking about the Philippine history and society,</p>
+            <p>examining how they align with the principles and ideals taught by José Rizal.</p>
           </div>
           <div className="border-t border-gray-200 pt-6">
-            <p className="text-gray-500">© {new Date().getFullYear()} All content for academic purposes.</p>
+            <p className="text-gray-500">© 2025. All content for academic purposes.</p>
           </div>
         </div>
       </div>
