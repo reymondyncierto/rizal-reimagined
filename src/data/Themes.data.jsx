58228ff0758920c5932ca703674f341b34e96177
Reymond Yncierto <rpyncierto@up.edu.ph>
2025-06-02 12:30:47 +0800

    chore: REMOVE last theme

--- src/data/Themes.data.jsx
+++ src/data/Themes.data.jsx
@@ -48,11 +48,11 @@ export const themes = [
       brief: "Standing for truth despite risks",
       color: "bg-blue-100 text-blue-800"
     },
-    {
-      id: 8,
-      title: "Global Perspective",
-      icon: "✈️",
-      brief: "Learning from the world while staying rooted",
-      color: "bg-teal-100 text-teal-800"
-    }
+    // {
+    //   id: 8,
+    //   title: "Global Perspective",
+    //   icon: "✈️",
+    //   brief: "Learning from the world while staying rooted",
+    //   color: "bg-teal-100 text-teal-800"
+    // }
 ]
